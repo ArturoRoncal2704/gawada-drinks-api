ac167f175e647dca72a324c0b480b5edf4e1b27c
Lisbeth <yoselynespinoza27@gmail.com>
2025-07-30 20:31:19 -0500

    Agregar controladores CRUD para Producto, Categoria, Marca, Pedido y Usuario

---
+++ ApiGawada/Controllers/DetallePedidoController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiGawada.Context;
+using ApiGawada.Models;
+
+namespace ApiGawada.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetallePedidoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DetallePedidoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DetallePedido
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DetallePedido>>> GetDetallesPedidos()
+        {
+            return await _context.DetallesPedidos.ToListAsync();
+        }
+
+        // GET: api/DetallePedido/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DetallePedido>> GetDetallePedido(int id)
+        {
+            var detallePedido = await _context.DetallesPedidos.FindAsync(id);
+
+            if (detallePedido == null)
+            {
+                return NotFound();
+            }
+
+            return detallePedido;
+        }
+
+        // PUT: api/DetallePedido/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDetallePedido(int id, DetallePedido detallePedido)
+        {
+            if (id != detallePedido.PedidoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(detallePedido).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetallePedidoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DetallePedido
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DetallePedido>> PostDetallePedido(DetallePedido detallePedido)
+        {
+            _context.DetallesPedidos.Add(detallePedido);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DetallePedidoExists(detallePedido.PedidoId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDetallePedido", new { id = detallePedido.PedidoId }, detallePedido);
+        }
+
+        // DELETE: api/DetallePedido/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDetallePedido(int id)
+        {
+            var detallePedido = await _context.DetallesPedidos.FindAsync(id);
+            if (detallePedido == null)
+            {
+                return NotFound();
+            }
+
+            _context.DetallesPedidos.Remove(detallePedido);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DetallePedidoExists(int id)
+        {
+            return _context.DetallesPedidos.Any(e => e.PedidoId == id);
+        }
+    }
+}
---
+++ ApiGawada/Controllers/MarcaController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiGawada.Context;
+using ApiGawada.Models;
+
+namespace ApiGawada.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarcaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MarcaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Marca
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Marca>>> GetMarcas()
+        {
+            return await _context.Marcas.ToListAsync();
+        }
+
+        // GET: api/Marca/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Marca>> GetMarca(int id)
+        {
+            var marca = await _context.Marcas.FindAsync(id);
+
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return marca;
+        }
+
+        // PUT: api/Marca/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMarca(int id, Marca marca)
+        {
+            if (id != marca.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(marca).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MarcaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Marca
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Marca>> PostMarca(Marca marca)
+        {
+            _context.Marcas.Add(marca);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMarca", new { id = marca.Id }, marca);
+        }
+
+        // DELETE: api/Marca/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMarca(int id)
+        {
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            _context.Marcas.Remove(marca);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MarcaExists(int id)
+        {
+            return _context.Marcas.Any(e => e.Id == id);
+        }
+    }
+}
