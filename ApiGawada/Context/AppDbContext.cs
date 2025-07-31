0cce1a492e8492556cf372575c03de01617ee714
Lisbeth <yoselynespinoza27@gmail.com>
2025-07-30 20:22:25 -0500

    Agregar AppDbContext con DbSet de entidades base

---
+++ ApiGawada/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ApiGawada.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGawada.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<DetallePedido> DetallesPedidos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Definir clave compuesta para DetallePedido
+            modelBuilder.Entity<DetallePedido>()
+                .HasKey(dp => new { dp.PedidoId, dp.ProductoId });
+        }
+    }
+}
